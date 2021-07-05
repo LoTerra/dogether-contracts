c26bdc068cdbb05431a8df74bdf6c3a5ef1ef9ad
0xantman <antho74830@gmail.com>
2021-07-05 14:28:03 +0200

    work in progress..

--- contracts/cw20-contract-dogether/src/state.rs
+++ contracts/cw20-contract-dogether/src/state.rs
@@ -1,7 +1,7 @@
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
-use cosmwasm_std::{Addr, CanonicalAddr, Uint128};
+use cosmwasm_std::{Addr, Uint128};
 use cw20::AllowanceResponse;
 use cw_storage_plus::{Item, Map};
 
