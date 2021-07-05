142b97849f632772020f8bb8e3d1b5e510d1ca77
0xantman <antho74830@gmail.com>
2021-07-05 17:53:57 +0200

    work in progress...

--- contracts/dogether/src/contract.rs
+++ contracts/dogether/src/contract.rs
@@ -1,4 +1,8 @@
-use cosmwasm_std::{attr, entry_point, to_binary, BankMsg, Binary, CanonicalAddr, Coin, ContractResult, CosmosMsg, Decimal, Deps, DepsMut, Env, Fraction, MessageInfo, Reply, Response, StdError, StdResult, SubMsg, SubcallResponse, Uint128, Uint64, WasmMsg, WasmQuery, ReplyOn};
+use cosmwasm_std::{
+    attr, entry_point, to_binary, BankMsg, Binary, CanonicalAddr, Coin, ContractResult, CosmosMsg,
+    Decimal, Deps, DepsMut, Env, Fraction, MessageInfo, Reply, ReplyOn, Response, StdError,
+    StdResult, SubMsg, SubcallResponse, Uint128, Uint64, WasmMsg, WasmQuery,
+};
 
 use crate::error::ContractError;
 use crate::math::{
@@ -698,7 +702,7 @@ mod tests {
         )
     }
     #[test]
-    fn un_pool(){
+    fn un_pool() {
         let mut deps = mock_dependencies(&[]);
         default_init(deps.as_mut());
         let info = mock_info("addr0000", &[]);
@@ -716,27 +720,35 @@ mod tests {
         };
         let res = reply(deps.as_mut(), env.clone(), rep).unwrap();
 
-        let msg = ExecuteMsg::UnPool { amount: Uint128(1_000) };
+        let msg = ExecuteMsg::UnPool {
+            amount: Uint128(1_000),
+        };
         let res = execute(deps.as_mut(), env.clone(), info.clone(), msg).unwrap();
 
-        let un_bond_msg = loterra_staking_contract_dogether::msg::ExecuteMsg::UnbondStake { amount:  Uint128(1_000), address: "addr0000".to_string() };
+        let un_bond_msg = loterra_staking_contract_dogether::msg::ExecuteMsg::UnbondStake {
+            amount: Uint128(1_000),
+            address: "addr0000".to_string(),
+        };
         println!("{:?}", res);
 
-        assert_eq!(res.messages, vec![CosmosMsg::Wasm(WasmMsg::Execute {
-            contract_addr: "staking".to_string(),
-            msg: to_binary(&un_bond_msg).unwrap(),
-            send: vec![]
-        })]);
+        assert_eq!(
+            res.messages,
+            vec![CosmosMsg::Wasm(WasmMsg::Execute {
+                contract_addr: "staking".to_string(),
+                msg: to_binary(&un_bond_msg).unwrap(),
+                send: vec![]
+            })]
+        );
 
         let msg = ExecuteMsg::UnPool { amount: Uint128(0) };
         let res = execute(deps.as_mut(), env, info, msg);
         match res {
-           Err(ContractError::EmptyAmount {}) => {}
-            _ => panic!("Do not enter here")
+            Err(ContractError::EmptyAmount {}) => {}
+            _ => panic!("Do not enter here"),
         }
     }
     #[test]
-    fn claim_un_pool(){
+    fn claim_un_pool() {
         let mut deps = mock_dependencies(&[]);
         default_init(deps.as_mut());
         let info = mock_info("addr0000", &[]);
@@ -756,24 +768,30 @@ mod tests {
 
         let res = try_claim_un_pool(deps.as_mut(), env, info).unwrap();
         println!("{:?}", res);
-        let msg = loterra_staking_contract_dogether::msg::ExecuteMsg::WithdrawStake { cap: None, address: "addr0000".to_string() };
+        let msg = loterra_staking_contract_dogether::msg::ExecuteMsg::WithdrawStake {
+            cap: None,
+            address: "addr0000".to_string(),
+        };
         let wasm_msg = WasmMsg::Execute {
             contract_addr: "staking".to_string(),
             msg: to_binary(&msg).unwrap(),
-            send: vec![]
+            send: vec![],
         };
-        let submessage = SubMsg{
+        let submessage = SubMsg {
             id: 2,
             msg: CosmosMsg::Wasm(wasm_msg),
             gas_limit: None,
-            reply_on: ReplyOn::Success
+            reply_on: ReplyOn::Success,
         };
-        assert_eq!(res, Response{
-            submessages: vec![submessage],
-            messages: vec![],
-            attributes: vec![],
-            data: None
-        })
+        assert_eq!(
+            res,
+            Response {
+                submessages: vec![submessage],
+                messages: vec![],
+                attributes: vec![],
+                data: None
+            }
+        )
     }
     #[test]
     fn redeem_earning() {
